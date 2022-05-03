a92824b5160812d275cf2414d3ab11ddfe8475a1
KOREAN139 <korean139@gmail.com>
2022-05-03 17:19:23 +0900

    feat(util): add log analysis precessing script

---
+++ utils/log.ts
@@ -0,0 +1,27 @@
+import { FourPlayerGameStat, RawFourPlayerGameStats } from '../models/log-analysis';
+import logAnalysis from '../public/log-analysis.json';
+
+export const getFourPlayerGameStats = (): FourPlayerGameStat[] => {
+	const fourPlayerGameLogs = logAnalysis["4player"] as RawFourPlayerGameStats;
+	const fourPlayerGameStats = Object.keys(fourPlayerGameLogs).map((player, i) => {
+		const gameStat = fourPlayerGameLogs[player];
+		const numOfFirst = gameStat["1"];
+		const numOfSecond = gameStat["2"];
+		const totalGames = gameStat["total_games"];
+
+		return {
+			player,
+			"rank": i+1,
+			"firstRatio": numOfFirst / totalGames * 100,
+			"upperSecondRatio": (numOfFirst + numOfSecond) / totalGames * 100,
+			totalGames,
+		};
+	});
+
+	const statSortFn = (playerA: FourPlayerGameStat, playerB: FourPlayerGameStat) => {
+	  return playerB.firstRatio - playerA.firstRatio;
+	};
+	fourPlayerGameStats.sort(statSortFn);
+
+	return fourPlayerGameStats;
+};
