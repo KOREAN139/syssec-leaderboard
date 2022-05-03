27976314e9e315a268ad3a57429dd0f017867937
KOREAN139 <korean139@gmail.com>
2022-05-03 23:17:10 +0900

    fix(build): add asset prefix to serve static files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: isProd ? 'https://KOREAN139.github.io/syssec-leaderboard/' : '',
 }
 
 module.exports = nextConfig
