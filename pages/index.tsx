56e6f8842389dbc97b4e49156129f20fffe8efd2
KOREAN139 <korean139@gmail.com>
2022-05-04 02:18:16 +0900

    refactor(page): get font via cdn

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,7 @@ const Home: NextPage = () => {
         <title>Syssec 마작 리더보드</title>
         <meta name="description" content="Syssec majsoul leaderboard" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" as="style" crossOrigin="true" href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/variable/pretendardvariable.css" />
       </Head>
 
       <main className={styles.main}>
