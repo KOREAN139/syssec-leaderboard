9b1e71ebc23e55b9afd34fdd06ed40647fbf8268
KOREAN139 <korean139@gmail.com>
2022-05-03 17:18:44 +0900

    feat(model): add model for log analysis

---
+++ models/log-analysis.ts
@@ -0,0 +1,32 @@
+export interface RawLogAnalysis {
+	"3player": RawThreePlayerGameStats;
+	"4player": RawFourPlayerGameStats;
+	uuids: number[];
+}
+
+export interface RawThreePlayerGameStats {
+	[nickname: string]: RawThreePlayerGameStat;
+}
+
+export interface RawThreePlayerGameStat {
+	total_games: number;
+	"1": number;
+	"2": number;
+	"3": number;
+}
+
+export interface RawFourPlayerGameStats {
+	[nickname: string]: RawFourPlayerGameStat;
+}
+
+export interface RawFourPlayerGameStat extends RawThreePlayerGameStat {
+	"4": number;
+}
+
+export interface FourPlayerGameStat {
+	player: string;
+	rank: number;
+	firstRatio: number;
+	upperSecondRatio: number;
+	totalGames: number;
+}
