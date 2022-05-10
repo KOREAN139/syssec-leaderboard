fe1b1377d018f2f40637beeca1c8aa639c503e0e
Sanggu Han <korean139@gmail.com>
2022-05-10 16:20:04 +0900

    feat(scraper): UUID 스크래핑 userscript 작성 (#16)

---
+++ scrape-game-uuid.js
@@ -0,0 +1,54 @@
+// ==UserScript==
+// @name         scrape-majsoul-game-uuid
+// @version      0.0.1
+// @author       Sanggu Han
+// @description  Copies uuid of friendly games in mahjong soul
+// @supportURL   https://github.com/KOREAN139/syssec-leaderboard/issues
+// @namespace    mjuuidscraper
+// @match        https://game.mahjongsoul.com/*
+// ==/UserScript==
+
+(function () {
+  const L_KEY_CODE = "KeyL";
+  const FRIENDLY_GAME_TYPE = 1;
+
+  document.addEventListener("keydown", e => {
+    e = e || window.event;
+    if (e.code != L_KEY_CODE) {
+      return;
+    }
+
+    waitAppLoading();
+    fetchLogList();
+  });
+
+  const waitAppLoading = () => {
+    if (typeof app === "undefined" || !app.NetAgent || !app.NetAgent.prototype) {
+      return setTimeout(() => waitAppLoading(), 1000);
+    }
+    console.log("app loaded");
+  }
+
+  const fetchLogList = () => {
+    console.log("fetchGameRecordList requested");
+    app.NetAgent.sendReq2Lobby(
+      "Lobby",
+      "fetchGameRecordList",
+      { start: 0, count: 100, type: FRIENDLY_GAME_TYPE },
+      (err, res) => {
+        if (err || res.error) {
+          console.log("error", err, res.error);
+          return;
+        }
+        console.log("fetchGameRecordList successed");
+        const uuids = res.record_list.map(r => r.uuid);
+        makePopup(uuids);
+      });
+  };
+
+  const makePopup = uuids => {
+    let popup = open("", "gameUuids", "_blank", "width=200,height=200");
+    popup.document.write(uuids.join('\n'));
+    popup.document.body.style.whiteSpace="pre-line";
+  };
+})();
