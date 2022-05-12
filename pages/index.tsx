984d465d8fd02a688337c6633fef19fe04c0e3a0
Sanggu Han <korean139@gmail.com>
2022-05-12 15:19:22 +0900

    refactor(page): 역만 테이블 구조 수정 (#26)
    * chore(page): adjust indent
    
    * refactor(page): adjust leaderboard table width
    
    * refactor(page): adjust yakuman table style

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,57 +8,52 @@ const Home: NextPage = () => {
 
   return (
     <div className={styles.container}>
-        <h1 className={styles.title}>
-          Syssec 마작 리더보드
-        </h1>
+      <h1 className={styles.title}>
+        Syssec 마작 리더보드
+      </h1>
 
-        <table className={styles.leaderboard}>
-          <thead>
-              <tr>
-                <th>순위</th>
-                <td>이름</td>
-                <td>1%<sup>*</sup></td>
-                <td>12%<sup>**</sup></td>
-                <td>국수</td>
-            </tr>
-          </thead>
-          <tfoot>
+      <table className={styles.leaderboard}>
+        <thead>
+          <tr>
+            <th>순위</th>
+            <td>이름</td>
+            <td>1%<sup>*</sup></td>
+            <td>12%<sup>**</sup></td>
+            <td>국수</td>
+          </tr>
+        </thead>
+        <tfoot>
+          <tr>
+            <td colSpan={6}>
+              * 1%: 전체 국수 중 1위를 한 국수의 비율 <br />
+              ** 12%: 전체 국수 중 1, 2위를 한 국수의 비율
+            </td>
+          </tr>
+        </tfoot>
+        <tbody>
+          {fourPlayerGameStates.map(stat => (
+            <tr key={`leader-table-${stat.rank}`}>
+              <th>{stat.rank}</th>
+              <td className={styles.nickname}>{stat.player}</td>
+              <td>{+stat.firstRatio.toFixed(1)}</td>
+              <td>{+stat.upperSecondRatio.toFixed(1)}</td>
+              <td>{stat.totalGames}</td>
+            </tr>))}
+        </tbody>
+      </table>
+
+      {yakumanHistories.map(({ yakumans, nickname, hand, huTile }, i) => (
+        <table className={styles.yakumanboard} key={`yakuman-${i}`}>
+          <tbody>
             <tr>
-              <td colSpan={6}>
-                * 1%: 전체 국수 중 1위를 한 국수의 비율 <br/>
-                ** 12%: 전체 국수 중 1, 2위를 한 국수의 비율
-              </td>
+              <th colSpan={2}>{nickname}</th>
             </tr>
-          </tfoot>
-          <tbody>
-            {fourPlayerGameStates.map(stat => (
-              <tr key={`leader-table-${stat.rank}`}>
-                <th>{stat.rank}</th>
-                <td>{stat.player}</td>
-                <td>{+stat.firstRatio.toFixed(1)}</td>
-                <td>{+stat.upperSecondRatio.toFixed(1)}</td>
-                <td>{stat.totalGames}</td>
-              </tr>))}
-          </tbody>
-        </table>
-
-        <table className={styles.yakumanboard}>
-          <thead>
-              <tr>
-                <th>이름</th>
-                <td>역만</td>
-                <td>패</td>
+            <tr className={styles.yakuman}>
+              <td className={styles.name}>{yakumans.join("\n")}</td>
+              <td className={styles.hand}>{hand.join("") + " " + huTile}</td>
             </tr>
-          </thead>
-          <tbody>
-            {yakumanHistories.map(({ yakumans, nickname, hand, huTile }, i) => (
-              <tr key={`yakuman-table-${i}`}>
-                <th>{nickname}</th>
-                <td className={styles.yakuman}>{yakumans.join(" ")}</td>
-                <td>{hand.join("") + " " + huTile}</td>
-              </tr>))}
           </tbody>
-        </table>
+        </table>))}
     </div>
   )
 }
