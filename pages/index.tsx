257d1f6acb0e2658bcad119c34b848e752db1496
KOREAN139 <korean139@gmail.com>
2022-05-12 16:27:17 +0900

    refactor(component): leaderboard as component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,46 +1,16 @@
 import type { NextPage } from 'next'
 import styles from '@/styles/Home.module.css'
-import { getFourPlayerGameStats } from '@/utils/log'
 import YakumanBoard from '@/components/yakuman-board';
+import Leaderboard from '@/components/leaderboard';
 
 const Home: NextPage = () => {
-  const fourPlayerGameStates = getFourPlayerGameStats();
-
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>
         Syssec 마작 리더보드
       </h1>
 
-      <table className={styles.leaderboard}>
-        <thead>
-          <tr>
-            <th>순위</th>
-            <td>이름</td>
-            <td>1%<sup>*</sup></td>
-            <td>12%<sup>**</sup></td>
-            <td>국수</td>
-          </tr>
-        </thead>
-        <tfoot>
-          <tr>
-            <td colSpan={6}>
-              * 1%: 전체 국수 중 1위를 한 국수의 비율 <br />
-              ** 12%: 전체 국수 중 1, 2위를 한 국수의 비율
-            </td>
-          </tr>
-        </tfoot>
-        <tbody>
-          {fourPlayerGameStates.map(stat => (
-            <tr key={`leader-table-${stat.rank}`}>
-              <th>{stat.rank}</th>
-              <td className={styles.nickname}>{stat.player}</td>
-              <td>{+stat.firstRatio.toFixed(1)}</td>
-              <td>{+stat.upperSecondRatio.toFixed(1)}</td>
-              <td>{stat.totalGames}</td>
-            </tr>))}
-        </tbody>
-      </table>
+      <Leaderboard />
       <YakumanBoard />
     </div>
   )
