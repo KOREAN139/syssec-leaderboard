3a45946a42facd3c5181970ec39391a31a664dcc
KOREAN139 <KOREAN139@users.noreply.github.com>
2022-05-12 10:44:48 +0000

    deploy: 942a9bc3b8bc6d2b42b7966b299644cc1b03e5ba

--- _next/static/_SzSBng4FNN0eEdbuw2pC/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/3e576269467c64e4.css","static/chunks/pages/index-65709f062aa70b01.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/tj5zaA7MdeMftH83MdhBv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/2d94c12d8acf689e.css","static/chunks/pages/index-02fc1ccb359a47b1.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
