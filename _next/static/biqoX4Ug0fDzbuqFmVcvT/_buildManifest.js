1102e448e6e83bb72ffc651b7ba5cfc9a6e6408e
KOREAN139 <KOREAN139@users.noreply.github.com>
2022-05-12 06:20:13 +0000

    deploy: 984d465d8fd02a688337c6633fef19fe04c0e3a0

---
+++ _next/static/biqoX4Ug0fDzbuqFmVcvT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/3e934900d3242473.css","static/chunks/pages/index-1d225a8377843e2e.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
