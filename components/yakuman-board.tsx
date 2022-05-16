edb8cf45538b95ea9bdbd2aff49dca00c6e30b90
Sanggu Han <korean139@gmail.com>
2022-05-16 20:41:13 +0900

    feat(component): 역만 타일 이미지 렌더링 (#36)
    * chore(public): split tile images
    
    * feat(util): add string-tile image map
    
    * feat(component): render tile image
    
    * refactor(component): increase size & apply font to nickname
    
    * refactor(component): add unique key prop in Image

--- components/yakuman-board.tsx
+++ components/yakuman-board.tsx
@@ -1,7 +1,9 @@
 import type { NextComponentType } from 'next'
+import Image from 'next/image';
 import styles from '@/styles/YakumanBoard.module.css'
 import TableContainer from '@/components/table-container';
 import { getYakumanHistories } from '@/utils/log'
+import { tileImageMap } from '@/utils/tile-images';
 
 const YakumanBoard: NextComponentType = () => {
   const yakumanHistories = getYakumanHistories();
@@ -12,11 +14,31 @@ const YakumanBoard: NextComponentType = () => {
         <table className={styles.board} key={`yakuman-${i}`}>
           <tbody>
             <tr>
-              <th colSpan={2}>{nickname}</th>
+              <th colSpan={2} className={styles.nickname}>{nickname}</th>
             </tr>
             <tr className={styles.yakuman}>
               <td className={styles.name}>{yakumans.join("\n")}</td>
-              <td className={styles.hand}>{hand.join("") + " " + huTile}</td>
+              <td className={styles.hand}>
+                {hand.map((tile, i) => (
+                  <Image
+                    src={tileImageMap[tile]}
+                    key={`${i}-${tile}`}
+                    className={styles.tile}
+                    height="60%"
+                    width="37%"
+                    objectFit="contain"
+                    draggable={false}
+                  />
+                ))}
+                <Image
+                  src={tileImageMap[huTile]}
+                  className={styles.hutile}
+                  height="60%"
+                  width="60%"
+                  objectFit="contain"
+                  draggable={false}
+                />
+              </td>
             </tr>
           </tbody>
         </table>))}
