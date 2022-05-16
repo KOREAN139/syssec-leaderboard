e5e703eb7f540a5d0915b7a7e16cc8e3ae1fded6
KOREAN139 <korean139@gmail.com>
2022-05-16 20:31:58 +0900

    feat(component): render tile image

--- components/yakuman-board.tsx
+++ components/yakuman-board.tsx
@@ -1,7 +1,9 @@
 import type { NextComponentType } from 'next'
+import Image from 'next/image';
 import styles from '@/styles/YakumanBoard.module.css'
 import TableContainer from '@/components/table-container';
 import { getYakumanHistories } from '@/utils/log'
+import { tileImageMap } from '@/utils/tile-images';
 
 const YakumanBoard: NextComponentType = () => {
   const yakumanHistories = getYakumanHistories();
@@ -16,7 +18,26 @@ const YakumanBoard: NextComponentType = () => {
             </tr>
             <tr className={styles.yakuman}>
               <td className={styles.name}>{yakumans.join("\n")}</td>
-              <td className={styles.hand}>{hand.join("") + " " + huTile}</td>
+              <td className={styles.hand}>
+                {hand.map(tile => (
+                  <Image
+                    src={tileImageMap[tile]}
+                    className={styles.tile}
+                    height="60%"
+                    width="37%"
+                    objectFit="contain"
+                    draggable={false}
+                  />
+                ))}
+                <Image
+                  src={tileImageMap[huTile]}
+                  className={styles.hutile}
+                  height="60%"
+                  width="60%"
+                  objectFit="contain"
+                  draggable={false}
+                />
+              </td>
             </tr>
           </tbody>
         </table>))}
