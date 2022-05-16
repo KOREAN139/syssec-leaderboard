c34ecb4fb999b677dd1d88f5fe194c8f303b47af
KOREAN139 <korean139@gmail.com>
2022-05-16 21:01:52 +0900

    fix(app): change default image loader

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   reactStrictMode: true,
   assetPrefix: isProd ? 'https://KOREAN139.github.io/syssec-leaderboard/' : '',
+  images: {
+    loader: 'akamai',
+    path: '/',
+  },
 }
 
 module.exports = nextConfig
